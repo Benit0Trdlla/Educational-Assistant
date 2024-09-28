4a44f6ff86d8875c8efa3e5df1336193c6e59986
Benit0Trdlla <benilorenzo007op@gmail.com>
2024-09-28 15:27:51 -0300

    [New]: metadata tags

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -2,11 +2,32 @@ import "./globals.css";
 import { Metadata } from "next";
 import { Toaster } from "sonner";
 
+const APP_NAME = "Tu Asistente Educativo 🏫";
+const APP_DEFAULT_TITLE = "Tu Asistente Educativo 🏫";
+const APP_DESCRIPTION = "Soy tu asistente educativo que te ayudará con tus tareas y dudas académicas.";
+
 export const metadata: Metadata = {
-  metadataBase: new URL("https://ai-sdk-preview-attachments.vercel.dev"),
-  title: "Tu Asistente Educativo 🏫",
-  description: "Asistente educativo diseñado para ayudar a estudiantes de diversas edades con sus tareas y dudas académicas.",
+  metadataBase: new URL("https://educational-assistant.vercel.app/"),
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_DEFAULT_TITLE,
+  },
+  description: APP_DESCRIPTION,
   manifest: "/manifest.json",
+  appleWebApp: {
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_DEFAULT_TITLE,
+    },
+    description: APP_DESCRIPTION,
+  },
   icons: {
     icon: "/icon512_rounded.png",
   }
